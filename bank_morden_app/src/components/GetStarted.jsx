763c6aa0595673c0e445af9027f8587085c1e008
oletilwe12 <loatemophuting@gmail.com>
2022-08-26 04:04:49 +0200

    FIRST COMMIT

--- bank_morden_app/src/components/GetStarted.jsx
+++ bank_morden_app/src/components/GetStarted.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import styles from '../style';
+import { arowUp} from '../assets';
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
+const GetStarted = () =>  (
+    <div className={'${stylesflexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2'}>
+
+    </div>
   )
-}
 
 export default GetStarted
